bad9255ae9c2412f9c895f8466bd804d51a3ca12
Maxim <creativesun@yandex.ru>
2022-05-19 00:12:28 +0300

    feat(preamble): Listings parameters test

---
+++ 1.ts
@@ -0,0 +1,13 @@
+console.log('hello1')
+/*console.log('hello2')
+console.log('hello3')
+console.log('hello4')*/
+if (10 >= 5) {
+    console.log('blah nlag\n');
+}
+console.log('hello5')
+//console.log('hello6')
+console.log('hello7')
+console.log('hello8')
+console.log('hello9')
+console.log('hello10 hello10hello10hello10hello10 hello10hello10hello10hello10hello10')
